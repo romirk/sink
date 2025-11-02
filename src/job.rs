8093aed559b4af89f7d752677f47343752c6640f
romir kulshrestha <romir.kulshrestha@gmail.com>
2025-11-02 02:00:44 +0100

    blocked

--- src/job.rs
+++ src/job.rs
@@ -1,10 +1,19 @@
+use std::sync::mpsc::{Receiver, Sender};
 use rand::Rng;
 use std::thread::sleep;
 use std::time::Duration;
 
 pub type JobResult = Result<usize, usize>;
+
+pub enum Message {
+    Ok(usize),
+    Err(usize),
+    Blocked(usize, usize),
+    Unblocked,
+}
+
 /// Dummy worker
-pub fn busywork(id: usize) -> JobResult {
+pub fn busywork(id: usize, tx: Sender<Message>, rx: Receiver<Message>) -> JobResult {
     let mut rng = rand::rng();
 
     // simulate random effort
@@ -25,12 +34,34 @@ pub fn busywork(id: usize) -> JobResult {
         let should_error = rng.random::<u8>() == 0;
         if should_error {
             println!("[{id}] stop {i}");
+
+            tx.send(Message::Err(id)).unwrap();
             return Err(id);
+        }
+
+        let should_block = rng.random::<u8>() % 10 == 0;
+        if should_block {
+            let mut blocker = rng.random_range(0..10);
+            while blocker == id {
+                blocker = rng.random_range(0..10) ;
+            }
+            tx.send(Message::Blocked(id, blocker)).unwrap();
+            while let response = rx.recv().unwrap()  {
+                match response {
+                   Message::Unblocked => break,
+                    _ => {
+                        tx.send(Message::Err(id)).unwrap();
+                        return Err(id);
+                    }
+                }
+            }
+            println!("[{id}] unblocked");
         }
         println!("[{id}] {i}");
         sleep(interval);
     }
 
     println!("[{id}] end");
+    tx.send(Message::Ok(id)).unwrap();
     Ok(id)
 }
--- src/worker.rs
+++ src/worker.rs
@@ -1,5 +1,5 @@
-use crate::job::{busywork};
-use std::collections::HashMap;
+use crate::job::{busywork, Message};
+use std::collections::{HashMap, HashSet};
 use std::sync::mpsc;
 use std::thread;
 
@@ -9,29 +9,49 @@ pub fn run(jobs: usize) {
         .map(|id| {
             println!("starting worker thread");
             let tx = tx.clone();
-            thread::spawn(move || {
-                tx.send(busywork(id)).unwrap();
-            })
+            let (t2, r2) = mpsc::channel();
+            (
+                t2,
+                thread::spawn(move || {
+                    busywork(id, tx, r2);
+                }),
+            )
         })
+        .enumerate()
+        .collect::<HashMap<_, _>>();
+
+    let mut blockers = (0..jobs)
+        .map(|id| HashSet::new())
         .enumerate()
         .collect::<HashMap<_, _>>();
 
     while handles.len() > 0 {
         let result = rx.recv().unwrap();
-        let id;
         match result {
-            Ok(job) => {
+            Message::Ok(job) | Message::Err(job) => {
                 println!("[{job}] success");
-                id = job;
-            }
-            Err(job) => {
-                println!("[{job}] failed");
-                id = job;
-            }
-        }
+                let (_, handle) = handles.remove(&job).unwrap();
+                handle.join().unwrap();
 
-        let handle = handles.remove(&id).unwrap();
-        handle.join().unwrap();
+                let blocked_tasks = blockers.get_mut(&job).unwrap();
+                blocked_tasks.iter().for_each(|blocked| {
+                    let (tx, _) = handles.get_mut(blocked).unwrap();
+                    tx.send(Message::Unblocked).unwrap();
+                });
+                blocked_tasks.clear();
+            }
+            Message::Blocked(job, blocker) => {
+                println!("[{job}] blocked by {blocker}");
+
+                if handles.contains_key(&blocker) && !blockers[&job].contains(&blocker) {
+                    blockers.get_mut(&blocker).unwrap().insert(job);
+                } else {
+                    let (tx, _) = handles.get_mut(&job).unwrap();
+                    tx.send(Message::Unblocked).unwrap();
+                }
+            }
+            Message::Unblocked => unreachable!(),
+        }
     }
 
     println!("finished all tasks");
